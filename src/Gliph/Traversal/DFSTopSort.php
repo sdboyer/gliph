b44703198743ebe56ca8747ae8761740d486ef4f
Sam Boyer <tech@samboyer.org>
2013-08-16 22:53:48 -0400

    Remove old, shitty DFS/BFS implementations.

--- src/Gliph/Traversal/DFSTopSort.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Gliph\Traversal;
-
-class DFSTopSort extends BFSTopSort {
-    protected function nextVertex(\SplDoublyLinkedList $queue) {
-        return $queue->pop();
-    }
-}
