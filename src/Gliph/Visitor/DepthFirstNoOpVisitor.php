fa9c1a64f8e208c515fcfefb21dd477630083899
Sam Boyer <tech@samboyer.org>
2013-08-19 20:41:21 -0400

    Fix DepthFirstNoOpVisitor class name.

--- src/Gliph/Visitor/DepthFirstNoOpVisitor.php
+++ src/Gliph/Visitor/DepthFirstNoOpVisitor.php
@@ -5,7 +5,7 @@ namespace Gliph\Visitor;
 /**
  * A no-op visitor for depth first traversal algorithms.
  */
-class DFSNoOpVisitor implements DepthFirstVisitorInterface {
+class DepthFirstNoOpVisitor implements DepthFirstVisitorInterface {
     public function onInitializeVertex($vertex, $source, \SplQueue $queue) {}
     public function onBackEdge($vertex, \Closure $visit) {}
     public function onStartVertex($vertex, \Closure $visit) {}
