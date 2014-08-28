6d52f9a2ed1eaf1acc61e6bbf9ef0897d903257e
Sam Boyer <tech@samboyer.org>
2014-08-28 12:55:05 -0400

    Granulate MutableGraph into MutableVertexSet

---
+++ src/Gliph/Graph/MutableVertexSet.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Gliph\Graph;
+
+/**
+ * Interface describing a mutable vertex set.
+ *
+ * This is an atomic component interface for graphs.
+ */
+interface MutableVertexSet {
+
+    /**
+     * Adds a vertex to the graph.
+     *
+     * Gliph requires that its graph vertices be objects; beyond that, it does
+     * not care about vertex type.
+     *
+     * @param object $vertex
+     *   An object to use as a vertex in the graph.
+     *
+     * @return Graph
+     *   The current graph instance.
+     *
+     * @throws InvalidVertexTypeException
+     *   Thrown if an invalid type of data is provided as a vertex.
+     */
+    public function addVertex($vertex);
+
+    /**
+     * Remove a vertex from the graph.
+     *
+     * This will also remove any edges that include the vertex.
+     *
+     * @param object $vertex
+     *   A vertex object to remove from the graph.
+     *
+     * @return Graph
+     *   The current graph instance.
+     *
+     * @throws NonexistentVertexException
+     *   Thrown if the provided vertex is not present in the graph.
+     */
+    public function removeVertex($vertex);
+
+}
